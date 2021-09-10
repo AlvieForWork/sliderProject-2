33588a3918fdb1633b8d5957acff9cde44063d5d
Alvie <alvieforwork@gmail.com>
2021-09-10 21:26:15 +0800

    change color

--- sliderProject/ViewController.swift
+++ sliderProject/ViewController.swift
@@ -8,11 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var stayHome: UIImageView!
+    @IBOutlet weak var hand: UIImageView!
+    @IBOutlet weak var boy: UIImageView!
+    @IBOutlet weak var mask: UIImageView!
+    @IBOutlet weak var magnifier: UIImageView!
+    @IBOutlet weak var virus: UIImageView!
+    @IBOutlet weak var medicine1: UIImageView!
+    @IBOutlet weak var medicine2: UIImageView!
+    @IBOutlet weak var medicine3: UIImageView!
+    @IBOutlet weak var medicine4: UIImageView!
+    
+    
+    func updateLocation(degress:CGFloat){
+        magnifier.transform = CGAffineTransform.identity.rotated(by: CGFloat.pi/180*degress).translatedBy(x: 0 , y: -350)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateLocation(degress: 270)
     }
+    
+    
+    @IBAction func close(_ sender: UISlider) {
+        let speed = sender.value*0.73
+        boy.frame.origin.x = CGFloat(100+speed)
+        mask.frame.origin.x = CGFloat(700-speed)
+        virus.alpha = CGFloat(sender.value/360)
+        updateLocation(degress: CGFloat(sender.value/2)+270)
+        
+        medicine1.frame.origin.y = CGFloat(150-speed/3)
+        medicine2.frame.origin.y = CGFloat(200-speed/4)
+        medicine3.frame.origin.y = CGFloat(250-speed*2)
+        medicine4.frame.origin.y = CGFloat(300-speed/6)
+        
+        
+        
+
+    }
+    
 
 
 }
